f89a222144b3fd8929dfc4d6f00c260001877c7d
root <root@local>
2018-12-28 17:10:38 +0900

    プロジェクト ファイルを追加します。

---
+++ AntlrSimpleCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AntlrSimpleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
